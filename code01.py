5fd23a7a80c7097adba1b56ab668927c8d8b13b5
yunyu ! <69829908+chuayunyu@users.noreply.github.com>
2020-09-29 10:00:22 +0800

    Create code01.py

---
+++ code01.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+print("Hello " + name )
+age = input("What is your age?")
+age_in_days = int(age) * 365
+print("You are " + age + " years old. You’ve lived for more than " + str(age_in_days) + " days!")
+
+
+[there's apparently something wrong with the forth line but i copied it exactly from the slides??]
